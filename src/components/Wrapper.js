279869c0f78dc20d1194da6259b94e2883f86293
biokah <maria.anaisr@gmail.com>
2018-04-23 19:19:01 -0500

    WIP filter coupons

--- src/components/Wrapper.js
+++ src/components/Wrapper.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Coupons from './Coupons'
 import Partners from './Partners'
+import Filter from './Filter'
 
 class Wrapper extends Component {
     constructor(){
@@ -9,7 +10,34 @@ class Wrapper extends Component {
             coupons: [],
             partners: []
         }
+        this.sortCoupon = this.sortCoupon.bind(this)
     }
+
+    sortCoupon(event){
+        let newCoupons = [];
+        switch(event.target.value){
+            case 'mostExpensive':
+            newCoupons = this.state.coupons.sort((a, b) =>{
+                return b.price - a.price})
+            this.setState({coupons : newCoupons})
+            break;
+            case 'lessExpensive':
+            newCoupons = this.state.coupons.sort((a, b) =>{
+                return a.price - b.price})
+            this.setState({coupons : newCoupons})
+            break;
+            case 'aToZ':
+            newCoupons = this.state.coupons.sort((a, b) => a.title.localeCompare(b.title));
+            this.setState({coupons : newCoupons})
+            break;
+            case 'ZToA':
+            newCoupons = this.state.coupons.sort((a, b) => b.title.localeCompare(a.title));
+            this.setState({coupons : newCoupons})
+            break;
+            }
+        }
+
+    
 
     fetchCoupons(){
         fetch('https://freetime-laboratoria.herokuapp.com/api/Coupons/allByCategory')
@@ -17,7 +45,7 @@ class Wrapper extends Component {
         return response.json()
         })
         .then((response) => {
-            this.setState({coupons: response.coupons}, ()=>{console.log(this.state)})
+            this.setState({coupons: response.coupons}, ()=>{console.log(this.state.coupons,'response')})
         })
     }   
 
@@ -44,7 +72,8 @@ class Wrapper extends Component {
     render() {
         return (
             <div>
-                <Coupons coupons={this.state.coupons}/>
+                <Filter />
+                <Coupons sortCoupon={this.sortCoupon} coupons={this.state.coupons}/>
                 <Partners partners={this.state.partners}/>
             </div>
         )
@@ -52,3 +81,4 @@ class Wrapper extends Component {
 }
 
 export default Wrapper;
+
